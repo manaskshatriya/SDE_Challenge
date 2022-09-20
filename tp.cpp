3118a7106c6e5351219fab0d1a2b5cac7391967f
Manas Kshatriya <manaskshatriya01@gmail.com>
2022-09-20 21:53:50 +0530

    dsa

---
+++ tp.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vec vector<ll>
+#define fori(i,a,n) for(int i=a;i<n;i++)
+#define vecp vector< pair<ll,ll> >
+#define pb push_back
+#define ma map<ll,ll>
+#define se set<ll>
+#define fi first
+#define sec second
+#define mod 1000000007
+
+
+void solve()
+{
+    
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve(); 
+    }
+    return 0;
+}
